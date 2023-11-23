b453a8084b8e6551def69b4a56b044299714f188
VManonmanian <125425396+VManonmanian@users.noreply.github.com>
2023-11-24 00:01:46 +0530

    change from browser

--- zoho/src/zoho/RightShiftAllZero.java
+++ zoho/src/zoho/RightShiftAllZero.java
@@ -1,7 +1,6 @@
 package zoho;
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class RightShiftAllZero {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
